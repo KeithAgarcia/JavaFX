059abe16f88bc70e3fcf320959fc508721510731
Keith Garcia <Keith@Keiths-MacBook.local>
2017-05-01 17:12:03 -0400

    Finished

---
+++ src/sample/Contact.java
@@ -0,0 +1,20 @@
+package sample;
+
+/**
+ * Created by Keith on 5/1/17.
+ */
+public class Contact {
+    String name;
+    String phone;
+    String email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+    @Override
+    public String toString() {
+        return name + phone + email;
+    }
+}
